9c2a26c4b2edb85cd200606e224cfa2f27815aaa
Carlos <carlossantana@Natalie-Morriss-MacBook-Pro.local>
2020-01-21 18:36:30 -0500

    cleaned up navbar and added hover effect

--- popspen/src/components/Navbar.js
+++ popspen/src/components/Navbar.js
@@ -2,6 +2,15 @@ import React from 'react'
 import styled from 'styled-components'
 
 
+const Header =styled.div`
+position: absolute;
+top: 10%;
+left: 50%;
+transform: translate(-50%, -50%);
+z-index: 2;
+`;
+
+
 const Title = styled.h1`
 font-family: 'Indie Flower', cursive;
 font-size: 50px;
@@ -11,29 +20,36 @@ justify-content: center;
 
 const Tab = styled.ul`
 display:flex;
-justify-content:space-around;
-list-style-type: none
+justify-content:space-between;
+width: 400px;  
+margin:5% 0%;
 `;
 
-const List = styled.li`
+const Button = styled.button`
+font-size:20px;
+padding:0%, 5%;
 border:1px solid black;
 border-radius:18%;
 font-family: 'Indie Flower', cursive;
-color:white;
-padding:1% 1%;
+outline:none;
+&:hover{
+        color:white;
+        background:black;
+    
+}
 `;
 
 class Navbar extends React.Component{
     render(){
         return (
-            <>
-            <Title>PopsPens</Title>
-            <Tab>
-                <List>Home</List>
-                <List>Prices</List>
-                <List>Catolog</List>
-            </Tab>
-            </>
+            <Header>
+                <Title>PopsPens</Title>
+                <Tab>
+                    <Button className='tabs'>Home</Button>
+                    <Button className='tabs'>Prices</Button>
+                    <Button className='tabs'>Catolog</Button>
+                </Tab>
+            </Header>
         )
     }
 }
