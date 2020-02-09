cc12e13cb80e8e325ad9adb1946f3aff723e5b40
Carlos <carlossantana@Natalie-Morriss-MacBook-Pro.local>
2020-02-08 20:03:56 -0500

    changed gitignore file

---
+++ popspen/build/precache-manifest.1f560ab425ec3dac644af4e64559a4a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a40eea0fba27e614e2b473e2696d971",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c7e97735dd13053bc0b",
+    "url": "/static/css/main.9e9a632b.chunk.css"
+  },
+  {
+    "revision": "d7748f8062af9c151d7a",
+    "url": "/static/js/2.6aa86bbf.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.6aa86bbf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c7e97735dd13053bc0b",
+    "url": "/static/js/main.df937678.chunk.js"
+  },
+  {
+    "revision": "b1c984786528028ce7c6",
+    "url": "/static/js/runtime-main.abeb127d.js"
+  },
+  {
+    "revision": "824d40cce3e52f928b3e5a44b4421c4e",
+    "url": "/static/media/backI.824d40cc.jpg"
+  },
+  {
+    "revision": "36a2219ada73ded80157ba699cfded51",
+    "url": "/static/media/penStyles.36a2219a.jpg"
+  }
+]);
